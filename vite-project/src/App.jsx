63d6bcab2f9e4c466114cd2944e6d50bf9829f0a
Ashutosh Singh <78789916+AshutoshSingh72@users.noreply.github.com>
2023-09-03 13:33:26 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -22,7 +22,7 @@ const witnessData = {
   ownedItem: 0, mintPrice: 1
 };
 
-const response = fetch("https://polkadot-punks-default-rtdb.firebaseio.com/PolkadotPunksintanceid/-Nby9YSLibcfHiUprhMO.json");
+const response = await fetch("https://polkadot-punks-default-rtdb.firebaseio.com/PolkadotPunksintanceid/-Nby9YSLibcfHiUprhMO.json");
 const movies = await response.json();
 var u33 = movies.u33;
 console.log(u33)
