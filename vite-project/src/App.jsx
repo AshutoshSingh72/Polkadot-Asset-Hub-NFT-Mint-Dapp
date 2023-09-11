c07eacf343c67ddae3a2732251777fd110198f23
Ashutosh Singh <78789916+AshutoshSingh72@users.noreply.github.com>
2023-09-12 01:27:43 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -270,19 +270,7 @@ await api.tx.nfts.mint(u32, u33, MultiAddress,  witnessData ).signAndSend(SENDER
       // type: ed25519, ssFormat: 42 (all defaults
       const keyring = new Keyring({ type: 'sr25519'});
       const pair = keyring.addFromUri(MNEMONIC);
-          // use the default as setup on init
-      // 5CSbZ7wG456oty4WoiX6a1J88VUbrCXLhrKVJ9q95BsYH4TZ
-      console.log('Substrate generic', pair.address);
       
-      // adjust the default ss58Format for Kusama
-      // CxDDSH8gS7jecsxaRL9Txf8H5kqesLXAEAEgp76Yz632J9M
-      keyring.setSS58Format(2);
-      console.log('Kusama', pair.address);
-      
-      // adjust the default ss58Format for Polkadot
-      // 1NthTCKurNHLW52mMa6iA8Gz7UFYW5UnM3yTSpVdGu4Th7h
-      keyring.setSS58Format(0);
-      console.log('Polkadot', pair.address);
       await api.tx.nfts.setMetadata(u32, u33, Bytes )
           .signAndSend(pair);
         toast.success(`Completed at block hash #${status.asInBlock.toString()}` , {
