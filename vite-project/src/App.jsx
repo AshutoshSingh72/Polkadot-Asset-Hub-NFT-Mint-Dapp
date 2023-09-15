7807cb1abbb3488d836d0abd394a03134aa25370
Ashutosh Singh <78789916+AshutoshSingh72@users.noreply.github.com>
2023-09-15 10:16:27 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -12,32 +12,6 @@ import { Keyring } from '@polkadot/keyring';
 
 
 
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 const NAME = 'Polkadot Punks';
 const u32 = "7";
 
@@ -406,14 +380,7 @@ theme="colored" />
 <div className='mintitem'>
 <button onClick={bol} className='mint' >Mint Item</button>
 </div>
-<button id="myBtn">open modal</button>
-<div id="myModal" class="modal">
-<div class="modal-content">
-    <span class="close">&times;</span>
-    <p>Some text in the Modal..</p>
-  </div>
 
-</div>
 <hr className='hr2'></hr>
 <small>©Polkadot Punks, 2023.</small>
 <div>
