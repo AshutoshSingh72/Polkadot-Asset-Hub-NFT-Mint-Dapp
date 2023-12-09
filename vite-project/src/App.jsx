43929577b771d88c61d91ea8e4e27eeb3d4c7ab0
Ashutosh Singh <78789916+AshutoshSingh72@users.noreply.github.com>
2023-12-09 23:52:55 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -43,7 +43,7 @@ function App() {
 
   const setup = async() => {
     //This wsProvider is for Polkadot based mint dapp for kusama change statemint to statemine only
-    const wsProvider = new WsProvider("wss://statemint.api.onfinality.io/ws?apikey=023fc078-a5b7-4a72-81c4-40e118b6097b");
+    const wsProvider = new WsProvider("wss://statemint.api.onfinality.io");
     const api = await ApiPromise.create({ provider: wsProvider})
     setApi(api);
   }
@@ -189,7 +189,7 @@ api.tx.utility.batchAll([ await api.tx.xcmPallet.limitedTeleportAssets(dest, ben
   //Mint Function 
 async function mint(){
   //kusama wsProvider wss://kusama-rpc.polkadot.io
-  const wsProvider = new WsProvider("wss://statemint.api.onfinality.io/ws?apikey=023fc078-a5b7-4a72-81c4-40e118b6097b");
+  const wsProvider = new WsProvider("wss://statemint.api.onfinality.io");
     const api = await ApiPromise.create({ provider: wsProvider})
     setApi(api);
   // (this needs to be called first, before other requests)
@@ -231,6 +231,7 @@ await api.tx.nfts.mint(u32, u33, MultiAddress,  witnessData ).signAndSend(SENDER
           progress: undefined,
           theme: "colored",
           });
+      
         u33++
 
                //test 
